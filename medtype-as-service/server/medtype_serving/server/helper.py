ef7d268d5d8ffd5decb0d70125db9a53bfbe9721
Vimig Socrates <vimig.socrates@gmail.com>
2021-04-02 23:10:17 -0500

    delete NTLogger

--- medtype-as-service/server/medtype_serving/server/helper.py
+++ medtype-as-service/server/medtype_serving/server/helper.py
@@ -8,9 +8,6 @@ __all__ = ['set_logger', 'get_args_parser', 'auto_bind', 'mergeList', 'clean_tex
 
 
 def set_logger(context, verbose=False):
-	if os.name == 'nt':  # for Windows
-		return NTLogger(context, verbose)
-
 	logger = logging.getLogger(context)
 	logger.setLevel(logging.DEBUG if verbose else logging.INFO)
 	formatter = logging.Formatter(
@@ -22,25 +19,6 @@ def set_logger(context, verbose=False):
 	logger.handlers = []
 	logger.addHandler(console_handler)
 	return logger
-
-
-class NTLogger:
-	def __init__(self, context, verbose):
-		self.context = context
-		self.verbose = verbose
-
-	def info(self, msg, **kwargs):
-		print('I:%s:%s' % (self.context, msg), flush=True)
-
-	def debug(self, msg, **kwargs):
-		if self.verbose:
-			print('D:%s:%s' % (self.context, msg), flush=True)
-
-	def error(self, msg, **kwargs):
-		print('E:%s:%s' % (self.context, msg), flush=True)
-
-	def warning(self, msg, **kwargs):
-		print('W:%s:%s' % (self.context, msg), flush=True)
 
 
 def check_max_seq_len(value):
